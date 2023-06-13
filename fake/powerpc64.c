0e3f217ccb9ba90b4265ab87cd0761746ab4f71f
shurizzle <me@shurizzle.dev>
2023-06-13 12:52:44 +0200

    Fix ppc/ppc64

--- fake/powerpc64.c
+++ fake/powerpc64.c
@@ -2,8 +2,6 @@ __asm__(".symver clock_getres,__kernel_clock_getres@@LINUX_2.6.15");
 void clock_getres(void) {}
 __asm__(".symver clock_gettime,__kernel_clock_gettime@@LINUX_2.6.15");
 void clock_gettime(void) {}
-__asm__(".symver clock_gettime64,__kernel_clock_gettime64@@LINUX_5.11");
-void clock_gettime64(void) {}
 __asm__(".symver datapage_offset,__kernel_datapage_offset@@LINUX_2.6.15");
 void datapage_offset(void) {}
 __asm__(".symver get_syscall_map,__kernel_get_syscall_map@@LINUX_2.6.15");
@@ -15,7 +13,7 @@ void getcpu(void) {}
 __asm__(".symver gettimeofday,__kernel_gettimeofday@@LINUX_2.6.15");
 void gettimeofday(void) {}
 __asm__(".symver sigtramp_rt32,__kernel_sigtramp_rt32@@LINUX_2.6.15");
-void sigtramp_rt32(void) {}
+void sigtramp_rt64(void) {}
 __asm__(".symver sigtramp32,__kernel_sigtramp32@@LINUX_2.6.15");
 void sigtramp32(void) {}
 __asm__(".symver sync_dicache,__kernel_sync_dicache@@LINUX_2.6.15");
